a3be2c810d214b82e10a7de142fa51d4e078893d
admin <admin@vraney-mbp.thelamplighterschool.org>
2017-11-15 18:20:54 -0600

    jquery donezo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,18 @@ $( document ).ready(function(){
 
 //global variable	
 //generates random number
-	var numRand = Math.floor(Math.random()*101+19)
+	var numRand = Math.floor(Math.random()*101+19);
 //sets score to 0
-	var count= 0
+	var count= 0;
 //sets crystals to random value 1-4
-	var attack1 = Math.floor(Math.random()*11+1)
-	var attack2 = Math.floor(Math.random()*11+1)
-	var attack3 = Math.floor(Math.random()*11+1)
-	var attack4 = Math.floor(Math.random()*11+1)
+	var attack1 = Math.floor(Math.random()*11+1);
+	var attack2 = Math.floor(Math.random()*11+1);
+	var attack3 = Math.floor(Math.random()*11+1);
+	var attack4 = Math.floor(Math.random()*11+1);
 
 //wins and losses
 	var wins = 0;
-	var loss = 0;
+	var losses = 0;
 
 //displays random number
 	$(".goal").html("u gotta get this number: "+numRand);
@@ -22,37 +22,78 @@ $( document ).ready(function(){
 //displays wins
 	$(".wins").html("Wins: "+wins);
 	
-	$(".losses").html("Losses: "+loss);
+	$(".losses").html("Losses: "+losses);
 //reset game
 	function gameReset() {
-		var numRand = Math.floor(Math.random()*101+19)
+		numRand = Math.floor(Math.random()*101+19);
 		$(".goal").html("u gotta get this number: "+numRand);
-		var count = 0
-		var attack1 = Math.floor(Math.random()*11+1)
-		var attack2 = Math.floor(Math.random()*11+1)
-		var attack3 = Math.floor(Math.random()*11+1)
-		var attack4 = Math.floor(Math.random()*11+1)
+		attack1 = Math.floor(Math.random()*11+1);
+		attack2 = Math.floor(Math.random()*11+1);
+		attack3 = Math.floor(Math.random()*11+1);
+		attack4 = Math.floor(Math.random()*11+1);
+		count= 0;
+		$(".counter").html("yr score: "+count);
 		}
 //win screen
 	function win(){
 		$(".message").html("yr the man now, dog");
 		wins++;
 		$(".wins").html("Wins: "+wins);
-		gameReset()
+		gameReset();
 	}
 //loss screen
 	function loss(){
-		$(".message").html("u so disappointing")
-		loss++
-		$(".losses").html("Losses: "+loss)
+		$(".message").html("u so disappointing");
+		losses++;
+		$(".losses").html("Losses: "+losses);
+		gameReset();
 	}
 //click functions
 //button one
-	$(".button-one").on("click", function() {
+	$("#button-one").on("click", function() {
 		count = count + attack1;
-		console.log(count);
 		$(".counter").html("yr score: "+count);
-
+		if (count === numRand) {
+			win();
+		}
+		else if (count > numRand) {
+			loss();
+		}
 	})
+
+	$("#button-two").on("click", function() {
+		count = count + attack2;
+		$(".counter").html("yr score: "+count);
+		if (count === numRand) {
+			win();
+		}
+		else if (count > numRand) {
+			loss();
+		}
+	})
+
+	$("#button-three").on("click", function() {
+		count = count + attack3;
+		$(".counter").html("yr score: "+count);
+		if (count === numRand) {
+			win();
+		}
+		else if (count > numRand) {
+			loss();
+		}
+	})
+
+	$("#button-four").on("click", function() {
+		count = count + attack4;
+		$(".counter").html("yr score: "+count);
+		if (count === numRand) {
+			win();
+		}
+		else if (count > numRand) {
+			loss();
+		}
+	})
+	
+	gameReset()
 
 })
